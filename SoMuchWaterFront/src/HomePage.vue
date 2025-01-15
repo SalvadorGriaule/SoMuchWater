2ff6f0a0cc875cef7d800beb08ef293d66343cf4
SalvadorGriaule <morales.quentin@gmx.fr>
2025-01-15 16:55:38 +0100

    last change

--- SoMuchWaterFront/src/HomePage.vue
+++ SoMuchWaterFront/src/HomePage.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
 import { useFetch } from './assets/js/useFetch';
 import { useRoute, useRouter } from 'vue-router';
+import SelectItem from './components/SelectItem.vue'
+import { computed, ref } from 'vue';
 
 const router = useRouter();
 const route = useRoute();
 
-const { data, error } = useFetch("http://127.0.0.1:8000/")
+const { data, error } = useFetch("http://127.0.0.1:8000/waterprint/")
+let mainSelect = ref('')
+let mainSelect2 = ref('')
+
+const compareWP = computed(() => {
+    if(mainSelect.value != "" && mainSelect2.value != ""){
+        let { data:select , error:sError} = useFetch("http://127.0.0.1:8000/waterprint/"+mainSelect.value)
+        let { data:select2 , error:selEr2} = useFetch("http://127.0.0.1:8000/waterprint/"+mainSelect2.value)
+        return {select,select2}
+    }
+})
 
 </script>
 
 <template>
-    <div>
-        <p>{{ data.message }}</p>
-    </div>
+    <section class="bg-sky-300">
+        <h2 class="text-center text-2xl mb-2">Calculateur relative</h2>
+        <form action=""
+            class="flex flex-col items-center space-y-2 lg:flex-row lg:space-y-0 lg:space-x-2 lg:px-2 lg:pb-2">
+            <SelectItem :tab="data" @selected="(e) => mainSelect = e" />
+            <SelectItem :tab="data" @selected="(e) => mainSelect2 = e" />
+        </form>
+        <p class="text-center">{{ mainSelect }} {{ mainSelect2 }}</p>
+        <p>{{ compareWP }}</p>
+    </section>
+    <section>
+        <ul>
+            <li v-for="(elem) in data">
+                {{ elem.name }} consomme {{ elem.water_print }} Litre d'eau
+            </li>
+        </ul>
+    </section>
 </template>
---
+++ SoMuchWaterFront/src/components/SelectItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+    tab?: Array<Object>
+}>()
+defineEmits(["selected"])
+let selected = ref('')
+</script>
+
+<template>
+    <select @change="$emit('selected',selected)" v-model="selected" class="w-3/5">
+        <option disabled value="default">Selectionner un item</option>
+        <option v-for="(elem) in tab" :value="elem.id">
+            {{ elem.name }}
+        </option>
+    </select>
+</template>
