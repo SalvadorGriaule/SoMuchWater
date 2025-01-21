d960364733730b68b1413a42c9e5044184021d57
SalvadorGriaule <morales.quentin@gmx.fr>
2025-01-21 11:57:06 +0100

    change of 21/01

--- Scrappeur/scrappeur/scrapper.jl
+++ Scrappeur/scrappeur/scrapper.jl
@@ -1,18 +1,18 @@
 import HTTP
-using Gumbo, Cascadia, AbstractTrees
+using Gumbo, Cascadia, AbstractTrees, JSON
 
 if length(ARGS) == 2
     rx = r"[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,4}(\S*)?"
     if !isnothing(match(rx, ARGS[1]))
-        let href = "https://" * ARGS[1] * "/",impasHR = [], pastHR = [] , zoneSearch = "", select = Selector(ARGS[2]), data=open("scrap.html","a")
+        let href = "https://" * ARGS[1] * "/", impasHR = [], pastHR = [], zoneSearch = "", select = Selector(ARGS[2]), tabI::Number = 1
             zoneSearch = href
-            while !isnothing(href) 
+            while !isnothing(href)
                 if isnothing(findfirst(isequal(href), pastHR))
                     push!(pastHR, href)
                 else
-                    push!(impasHR,href)
+                    push!(impasHR, href)
                     for hr in pastHR
-                        if !isnothing(match(Regex("$zoneSearch"), hr)) && isnothing(findfirst(isequal(hr),impasHR))
+                        if !isnothing(match(Regex("$zoneSearch"), hr)) && isnothing(findfirst(isequal(hr), impasHR))
                             href = hr
                         end
                     end
@@ -21,35 +21,48 @@ if length(ARGS) == 2
                     r = HTTP.get(href)
                     r_parsed = parsehtml(String(r.body))
                     body = r_parsed.root[2]
-                    content = eachmatch(select,body)
+                    content = eachmatch(select, body)
                     # println("match with ", content)
                     if !isempty(collect(content))
-                        println("plus de 0 mais ",length(collect(content)))
+                        println("plus de 0 mais ", length(collect(content)))
                         try
-                            tableHTML::Array{Array} = []
-                            tableTd::Array{String} = []
+                            tableHTML::Array{Dict} = []
+                            tableTd::Dict = Dict()
+                            trFirst = 0
+                            arrayKey = []
+                            currentKey = 1
                             for elem in PreOrderDFS(content[1].children)
                                 println(typeof(elem))
                                 if typeof(elem) <: HTMLText
                                     println(elem.text)
                                 elseif typeof(elem) <: HTMLElement
                                     println(tag(elem))
-                                    if tag(elem) == :tr
-                                        if length(tableTd) != 0
-                                            push!(tableHTML,tableTd)
-                                            tableTd = []
-                                        end
+                                    if tag(elem) == :tr && (trFirst == 0 || trFirst == 1)
+                                        trFirst += 1
+                                    elseif tag(elem) == :tr && trFirst > 1
+                                        push!(tableHTML, tableTd)
+                                        tableTd = Dict()
+                                        currentKey = 1
+                                    elseif tag(elem) == :td && trFirst == 1
+                                        tableTd[nodeText(elem)] = ""
+                                        arrayKey = [keys(tableTd)...]
+                                        println(arrayKey)
                                     elseif tag(elem) == :td
-                                        push!(tableTd,nodeText(elem))
+                                        tableTd[arrayKey[currentKey]] = nodeText(elem)
                                         println(tableTd)
+                                        currentKey += 1
                                     end
                                 end
                             end
                             println(tableHTML)
+                            open(string("tab", tabI, ".json"), "w") do f
+                                JSON.print(f, Dict("data" => tableHTML), 4)
+                            end
+                            tabI += 1
                             # write(data,nodeText(content[1]))
                             # write(data,"\n")
                         catch e
-                            println("error in write ",e)
+                            println("error in write ", e)
                         end
                     end
                     for elem in PreOrderDFS(body)
@@ -68,13 +81,13 @@ if length(ARGS) == 2
                 end
                 println("current:", href)
             end
-        for hr in pastHR
-            println(hr)
-        end
+            for hr in pastHR
+                println(hr)
+            end
         end
     else
         println("lien invalide")
     end
-else 
+else
     println("nombre d'argument invalide")
 end
