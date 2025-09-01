525aaeaeb8343cbf8936935d0ddffdd34180a67e
SalvadorGriaule <morales.quentin@gmx.fr>
2025-09-01 22:45:25 +0200

    new linux distro

--- SoMuchWaterBack/config.py
+++ SoMuchWaterBack/config.py
@@ -1,10 +1,14 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(BaseSettings):
-    client_id: str
-    client_secret: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
-    model_config = SettingsConfigDict(env_file=".env")
+class Settings(BaseSettings):
+    client_id: str | None
+    client_secret: str | None
+    secret_key: str | None
+    algorithm: str | None
+    access_token_expire_minutes: int | None
+
+    model_config = SettingsConfigDict(env_file=DOTENV)
